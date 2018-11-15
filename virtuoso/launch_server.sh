0a03a5fe4d0f929d23072ace411c35103a097e54
gheinrich <Guilhem.Heinirch@inra.fr>
2018-11-15 15:20:46 +0100

    nothi_ng

--- virtuoso/launch_server.sh
+++ virtuoso/launch_server.sh
@@ -1,7 +1,7 @@
 sudo docker run --rm \
-    -p 8080:8890 -p 1111:1111 \
+    -p 8080:27015 -p 1111:1111 \
     -e DBA_PASSWORD=pic3.14 \
     -e SPARQL_UPDATE=true \
     -e DEFAULT_GRAPH=http://www.example.com/my-graph \
-    -v /home/heinrich/SYSTEM/servers/virtuoso/db:/data \
+    -v /home/system/databases/virtuoso/db:/data \
     -d tenforce/virtuoso
